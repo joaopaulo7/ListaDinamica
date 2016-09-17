2000c9973afef51641af065ac480616ff37310d6
joaopaulo7 <joaopauloff7@hotmail.com>
2016-09-17 09:33:09 -0300

    Update quick.hpp

--- quick.hpp
+++ quick.hpp
@@ -1,12 +1,64 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
 
-int compare (const void * a, const void * b)
-{
-    return ( *(int*)a - *(int*)b );
-}
+using namespace std;
 
-void quick(int* vet, int tamanho)
+void quick(particula* ini, int tamanho, particula* fimu)
 {
-    qsort (vet, tamanho, sizeof(int), compare);
+    if(tamanho % 2 != 0){
+        tamanho++;
+    }
+    particula* ondenados= fimu;
+    particula* contador = ini;
+    particula* pivo= ini;
+    for (int i = 0; i < tamanho/2; i++){
+        pivo = pivo->getProx();
+    }
+    int auxiliar;
+    while(contador != fimu)
+    {
+        if(contador->getItem() >= pivo->getItem() || contador == pivo)
+        {
+            ondenados = fimu;
+            while(ondenados->getItem() > pivo->getItem() && ondenados != contador){
+                ondenados = ondenados->getAnt();
+            }
+            if(ondenados == pivo &&  ondenados != contador && contador->getItem() != ondenados->getItem() ){
+                auxiliar= contador->getItem();
+                contador->setItem(ondenados->getItem());
+                ondenados->setItem(auxiliar);
+                pivo =contador;
+            } else if(contador == pivo && contador->getItem() != ondenados->getItem() ){
+                auxiliar= pivo->getItem();
+                pivo->setItem(ondenados->getItem());
+                ondenados->setItem(auxiliar);
+                pivo = ondenados;
+            }else if(contador->getItem() != ondenados->getItem()){
+                auxiliar= contador->getItem();
+                contador->setItem(ondenados->getItem());
+                ondenados->setItem(auxiliar);
+
+            }
+        }
+            contador = contador->getProx();
+
+    }
+
+    tamanho=0;
+    int tamanho2=0;
+    contador = ini;
+    while(contador != pivo){
+        tamanho2++;
+        contador = contador->getProx();
+    }
+    while(pivo != fimu){
+        tamanho++;
+        pivo = pivo->getProx();
+    }
+    if(tamanho2 > 1){
+        quick(ini, tamanho2, contador);
+    }
+    if(tamanho > 1){
+        quick(contador, tamanho, fimu);
+    }
+
 }
