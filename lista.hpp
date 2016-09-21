641889b91ae49b4fc9b91bc64f6293ab3ff920e9
joaopaulo7 <joaopauloff7@hotmail.com>
2016-09-21 16:28:16 -0300

    Update lista.hpp

--- lista.hpp
+++ lista.hpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 #include "particula.hpp"
 #include "quick.hpp"
 
@@ -8,65 +9,77 @@ class lista{
 
 public:
 
-    void mostrar(){
+    void const mostrar(){
     particula* modulo = alfa;
-        do{
-            cout<< modulo<<"||";
+        do
+        {
+            cout<< modulo << "||";
             modulo->revelar();
             modulo = modulo->getProx();
         }while(modulo != gama);
-        cout<< gama<<"||"; gama->revelar();
+        cout<< gama <<"||"; gama->revelar();
     }
 
     void inserirfim(int item){
         this->ordenada = false;
         particula* inseri;
         inseri = new particula(item);
-        if(tam==0)
+        if(tam == 0)
         {
             this->alfa = inseri;
             this->gama = inseri;
             this->tam++;
-        }else{
+        }else
+        {
             inseri->Ligar(gama, inseri);
             this->gama = inseri;
             this->tam++;
         }
     }
 
-
-    friend ostream& operator<<(std::ostream& saida, lista& entrada){ return saida; }
-
-
-    void const listarCome(){
-        particula* modulo = alfa;
-        cout<<"[ ";
-        do{
-            cout << modulo->getItem() << ", ";
-            modulo = modulo->getProx();
-        }while(modulo != gama);
-        cout << modulo->getItem()<<"]\n";
-    }
-
-
-    void const listarFim(){
+    string const listarFim(){
+        stringstream saida;
         particula* modulo = gama;
-        cout<<"[ ";
-        do{
-            cout << modulo->getItem() << ", ";
+        saida << "[ ";
+        do
+        {
+            saida << modulo->getItem() << ", ";
             modulo = modulo->getAnt();
         }while(modulo != alfa);
-        cout << modulo->getItem()<<"]\n";
+        saida << modulo->getItem() << "] ";
+        return saida.str();
     }
+
+
+    friend ostream& operator<<(std::ostream& saida, lista& entrada){
+        saida << entrada.listarCome();
+        return saida;
+    }
+
 
 
     void ordenarAsc(){
         quick(alfa, tam, gama);
     }
-
 private:
     int tam;
     particula* alfa;
     particula* gama;
     bool ordenada;
+
+    string const listarCome(){
+        stringstream saida;
+        particula* modulo = alfa;
+        saida << "[ ";
+        do
+        {
+            saida << modulo->getItem() << ", ";
+            modulo = modulo->getProx();
+        }while(modulo != gama);
+        saida << modulo->getItem() << "] ";
+        return saida.str();
+    }
+
+
+
 };
